76c8209c45e2e4d2c2e9778446ca07f332f188c2
Simon Wanner <simon.wanner@gmx.net>
2021-09-07 20:37:11 +0200

    Initial commit

---
+++ srv/minecraft/backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# config vars
+
+SRC="/srv/minecraft/$1/"
+SNAP="/srv/minecraft/backup/$1"
+OPTS="-rltgoi --delay-updates --delete --chmod=a-w --exclude-from=/srv/minecraft/backup/exclude.txt"
+MINCHANGES=20
+
+# run this process with real low priority
+
+ionice -c 3 -p $$
+renice +12  -p $$
+
+mkdir -p "$SNAP"
+
+# sync
+
+time rsync $OPTS "$SRC" "$SNAP/latest" >> "$SNAP/rsync.log"
+
+# check if enough has changed and if so
+# make a hardlinked copy named as the date
+
+COUNT=$( wc -l "$SNAP/rsync.log"|cut -d" " -f1 )
+if [ $COUNT -gt $MINCHANGES ] ; then
+  DATETAG=$(date +%Y-%m-%d)
+  if [ ! -e "$SNAP/$DATETAG" ] ; then
+    cp -al "$SNAP/latest" "$SNAP/$DATETAG"
+    chmod u+w "$SNAP/$DATETAG"
+    mv "$SNAP/rsync.log" "$SNAP/$DATETAG"
+    chmod u-w "$SNAP/$DATETAG"
+  fi
+fi
+
+if [ -x "$SRC/post-backup.sh" ] ; then
+  sudo -u minecraft "$SRC/post-backup.sh"
+fi
+
+echo tellraw @a '{"text": "Backup done!", "color": "green"}' > /srv/minecraft/input/$1
---
+++ srv/minecraft/server-lib.fish
@@ -0,0 +1,77 @@
+#!/usr/bin/fish
+set NAME (basename (pwd))
+set STDIN /srv/minecraft/input/$NAME
+
+set OTHER_JAVA_OPTS '-XX:+UnlockExperimentalVMOptions -XX:+FlightRecorder'
+set GC_OPTS '-XX:+UseG1GC -XX:+DisableExplicitGC -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=85 -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -XX:+UseLargePages -XX:LargePageSizeInBytes=2M -XX:+ParallelRefProcEnabled'
+#GCOPTS='-XX:+UseConcMarkSweepGC -XX:MaxGCPauseMillis=100'
+set HEAP_OPTS '-Xmx4094M -Xms4094M -XX:HeapBaseMinAddress=1 -XX:+HeapDumpOnOutOfMemoryError'
+set -x JAVA_HOME /usr/lib/jvm/adoptopenjdk-16-hotspot-amd64/ 
+set FABRIC_LOADER_VERSION 0.11.3
+
+function make-input
+  if test ! -p "$STDIN"
+    mkfifo $STDIN
+  end
+end
+
+function artifact
+  if test (count $argv) -gt 3
+    echo /srv/minecraft/jars/(echo $argv[1] | tr . /)/$argv[2]/$argv[3]/$argv[2]-$argv[3]-$argv[4].jar
+  else
+    echo /srv/minecraft/jars/(echo $argv[1] | tr . /)/$argv[2]/$argv[3]/$argv[2]-$argv[3].jar
+  end
+end
+
+function vanilla-jar
+  set JAR (artifact com.mojang minecraft $argv[1] server)
+  if test ! -e "$JAR"
+    set manifest_url (curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.versions[] | select(.id == "'$argv[1]'") | .url')
+    set server_jar_url (curl -s $manifest_url | jq -r '.downloads.server.url')
+    mkdir -p (dirname "$JAR")
+    curl -o "$JAR" "$server_jar_url"
+  end
+  echo $JAR
+end 
+
+function launch-vanilla
+  if test (count $argv) -gt 1
+    launch-jar (vanilla-jar $argv[1]) $argv[2..-1]
+  else
+    launch-jar (vanilla-jar $argv[1])
+  end
+end
+
+function setup-fabric
+  set intermediary_jar (artifact net.fabricmc intermediary $argv[1])
+  if test ! -e "$intermediary_jar"
+    mkdir -p (dirname "$intermediary_jar")
+    curl -o "$intermediary_jar" "https://maven.fabricmc.net/net/fabricmc/intermediary/$argv[1]/intermediary-$argv[1].jar"
+  end
+end
+
+function launch-fabric
+  setup-fabric "$argv[1]"
+  if test -e "mods-$argv[1]"
+    rm -rf mods
+    cp -r "mods-$argv[1]" mods
+  end
+  echo serverJar=(vanilla-jar "$argv[1]") > fabric-server-launcher.properties
+  set cp (artifact net.fabricmc fabric-server-launch $FABRIC_LOADER_VERSION):(artifact net.fabricmc intermediary "$argv[1]")
+  set mainClass net.fabricmc.loader.launch.server.FabricServerLauncher
+  if test (count $argv) -gt 1
+    launch-cp $cp $mainClass $argv[2..-1]
+  else
+    launch-cp $cp $mainClass
+  end
+end
+
+function launch-jar
+  make-input
+  exec bash -c "exec tail -f $STDIN | exec -a $NAME $JAVA_HOME/bin/java $OTHER_JAVA_OPTS $HEAP_OPTS $GC_OPTS -jar $argv nogui"
+end
+
+function launch-cp
+  make-input
+  exec bash -c "exec tail -f $STDIN | exec -a $NAME $JAVA_HOME/bin/java $OTHER_JAVA_OPTS $HEAP_OPTS $GC_OPTS -cp $argv nogui"
+end
